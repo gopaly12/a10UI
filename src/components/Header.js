c2782a66ae9ffd9137234c043c8458c0ac5cac19
gopaly12 <gopal.y12@gmail.com>
2018-05-04 12:22:51 +0530

    Layout changes
    layout code added

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,6 @@ class Header extends Component {
           border: '5px solid pink'
         };
       return (
-        <div>
           <div className="body">
               <nav className="navbar navbar-inverse">
                   <div className="container-fluid">
@@ -23,9 +22,9 @@ class Header extends Component {
                       <a className="navbar-brand" >
                         <img src="https://www.highcharts.com/media/templates/highsoft_2015/images/logo.svg" width="" height="40" alt="Logo" />
                       </a>
-                  </div>
-                  <div className="collapse navbar-collapse" id="myNavbar">
-                      <ul className="nav navbar-nav  mr-auto">
+                  </div> 
+                  <div  id="myNavbar">
+                      <ul className="nav navbar-nav">
                           <li className="nav-item  listStyle active"><Link to ='/'>Home</Link></li>
                           <li className="nav-item  listStyle"><Link to = 'PICharts'>PICharts</Link></li>
                           <li className="nav-item  listStyle"><Link to = '/BarCharts'>BarCharts</Link></li>
@@ -33,11 +32,10 @@ class Header extends Component {
                           <li className="nav-item  listStyle"><Link to = '/LineCharts'>LineCharts</Link></li>
                           <li className="nav-item  listStyle"><Link to = '/SemiCharts'>SemiCharts</Link></li>
                       </ul> 
-                      </div>
+                    </div>
                   </div>
               </nav> 
           </div>
-        </div>
       )
     }
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom';
-import App from '../components/App'
+import Main from '../components/Main'
+import {
+    HighchartsChart, Chart, withHighcharts, XAxis, YAxis, Title, Legend, ColumnSeries, SplineSeries, PieSeries
+  } from 'react-jsx-highcharts';
 
 const root=document.getElementById("root")
 
-render(<App/>,root)
+render(<Main/>,root)
